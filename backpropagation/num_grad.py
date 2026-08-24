55d43376a857b326a2f7b4cd60462b93c4ca830f
Abdelhamid-RH <Abdelhamidrahmouni2@gmail.com>
2026-08-25 00:26:58 +0100

    Added hyperbolic tangent support

--- backpropagation/num_grad.py
+++ backpropagation/num_grad.py
@@ -1,10 +1,15 @@
 """
-This is my version of autograd I made it using differentiation by adding a small error h = 0.001
+This is my version of autograd I made it using numerical differentiation by adding a small error h = 0.001
 recursively instead of analytical derivatives. While it correctly approximates the gradient which is 
-the objective of backpropagation it won't scale well to a large neural network without memory overhead 
-and floating-point issues I will be mainly using it to test and debug micrograd
-it also assumes that variables are not repeated in one function and only supports addition and multiplication
+the objective of backpropagation it won't scale well to a large neural network without memory overhead
+and floating-point issues, it also assumes that variables are not repeated in one function!! which is rarely the case 
+in loss functions
+I made it mainly as a proof of concept of backpropagation, and to use it to test and debug micrograd
+It supports addition, multiplication and the hyperbolic tangent
+
 """
+import math
+
 class Value:
     def __init__(self, data = 0, children = (), op = ''):
         self.data = data
@@ -30,6 +35,12 @@ class Value:
             other.isf = 0
             return out
 
+    def tanh(self):
+        self.isf = 0
+        x = self.data
+        out = Value((math.exp(x) - math.exp(-x)) / (math.exp(x) +  math.exp(-x)), (self,), "tanh")
+        return out
+
     #displaying object information
     def disp(self):
          if len(self._children) > 1:
@@ -42,18 +53,24 @@ class Value:
         h = 0.001
         if self.isf == 1:
             self.grad = 1
+
         if len(self._children) < 1:
             return
+        elif len(self._children) < 2:
+            if self._op == "tanh":
+                x = (self._children[0].data + h)
+                t = ((math.exp(x)) - (math.exp(-x))) / (math.exp(x) +  math.exp(-x))
+                self._children[0].grad = (t - self.data) / h
+                self._children[0].__cal_local_partials()
         else: 
             if self._op == "+":
                 self._children[0].grad = (((self._children[0].data + h) + self._children[1].data) - self.data) / h
                 self._children[1].grad = (((self._children[0].data) + self._children[1].data  + h) - self.data) / h
-            elif self._op == "*":
+            elif self._op == "*":       # elif and not else in case of future operations
                 self._children[0].grad = (((self._children[0].data + h) * self._children[1].data) - self.data) / h
                 self._children[1].grad = (((self._children[0].data) * (self._children[1].data + h)) - self.data) / h
-
-        self._children[0].__cal_local_partials()
-        self._children[1].__cal_local_partials()
+            self._children[0].__cal_local_partials()
+            self._children[1].__cal_local_partials()
 
     # applying the chain rule
     def __cal_global_partials(self):
@@ -64,18 +81,24 @@ class Value:
 
         if len(self._children) < 1:
             return
+        elif len(self._children) < 2 :
+            self._children[0].grad = self._children[0].grad * self.grad
+            self._children[0].__cal_global_partials()
         else:
             self._children[0].grad = self._children[0].grad * self.grad
             self._children[1].grad = self._children[1].grad * self.grad
+            self._children[0].__cal_global_partials()
+            self._children[1].__cal_global_partials()
 
-        self._children[0].__cal_global_partials()
-        self._children[1].__cal_global_partials()
+
 
     
     # collectting parial derivatives in one vector which is the gradient 
     def __collect_gradient(self):
-        if self._op == "":
+        if len(self._children) < 1:
             self.__gradient.append({self.label : round(self.grad,3)})
+        elif len(self._children) < 2:
+            self._children[0].__collect_gradient()
         else:
             self._children[0].__collect_gradient()
             self._children[1].__collect_gradient()
@@ -89,19 +112,17 @@ class Value:
 
 
 
-#testing
+# testing
 
-#variables
+# variables
 x = Value(5)
 y = Value(1/2)
 z = Value(3)
+c = Value(2) 
 
-#function
-f = x * (y + z)
+x.label, y.label, z.label, c.label = "x", "y", "z", "c"
 
-x.label, y.label, z.label = "x", "y", "z"
-
-
+# function
+f = x.tanh() * ((y + z) * c)
 
 print(f"Gradient of f: {f.backward()}")
-
